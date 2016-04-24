e528b6bac0b81bfd1d08dfeb972e4074a48225ae
hmenn <hmen.56@gmail.com>
2016-04-24 22:00:40 +0300

    copy

--- hw4_gFD_Thread/hw4.c
+++ hw4_gFD_Thread/hw4.c
@@ -11,38 +11,55 @@
 #include <sys/types.h>
 #include "hw4.h"
 
-/* global variables */
-pid_t *pPid_childs=NULL;
-FILE *pFile_log=NULL;
-DIR *pDir_current=NULL;
+/* global degiskenler */
+t_child *pTChilds=NULL;  // fork ile olusturulalar
+DIR *pDir_current=NULL; // aktif klasor pointeri
 struct dirent * pDirent_current=NULL;
 
-pthread_t * pTh_thread=NULL;
-t_search *allSearch=NULL;
+pthread_t * pTh_thread=NULL; // olsuturulan threadler
+t_search *allSearch=NULL; // her dosya icin bilgi saklar
 
-char **strDirectories=NULL;
-char **strFiles=NULL;
+char **strDirectories=NULL; // tum klasor adresleri store edildi threadler icin
+char **strFiles=NULL; // tum dosya sayilari threadler icin store edildi
 
-int iFile_num=0;
-int iDir_num=0;
+int iFile_num=0; // bir klasordeki dosya sayisi
+int iDir_num=0; // bir klasordeki ic klasor sayisi
+
+// dizi icindeki id lere gore pipe olusturur
+int openFifoConnection(t_child *arr,int size,int drStatus){
+
+  char fifoName[MAX_FILE_NAME];
+
+  if(NULL == arr || size<=0 || drStatus<0 || drStatus >=size )
+    return 0;
+
+    /* childin pid si ve status ile acacak*/
+ 	pid_t pid = getpid();
+ 	sprintf(fifoName,"%ld-%d.ff",(long)pid,drStatus);
+
+	#ifdef DEBUG
+	 printf("Fifo : %s created.\n",fifoName);
+	#endif
+	  if(-1 == mkfifo(fifoName,FIFO_PERMS)){
+	    if(errno != EEXIST){
+	      fprintf(stderr,"FIFO ERROR : %s",strerror(errno));
+	      exit(1);
+	    }
+	  }
+  return 1;
+}
 
 
 
 
 int main(int argc,char *argv[]){
 
-
+	// arguman kontrol
 	if(argc!=3){
 		fprintf(stderr,"Main arguments error.\n");
 		fprintf(stderr,"USAGE : ./gfdT dirname word");
 		exit(EXIT_FAILURE);
 	}
-
-	signal(SIGINT,sigint_handler);
-
-	// open log file and check error
-	
-	struct sigaction action;
 
 
 	int iTotal_wordnum = search_dir(argv[1],argv[2]);
@@ -56,46 +73,20 @@ int main(int argc,char *argv[]){
 }
 
 
-// sigint handler
-void sigint_handler(int signum){
-
-	if(NULL != pFile_log){ // open log file
-		pFile_log = fopen(LOG_FILE_NAME,"w");
-		if(NULL == pFile_log){ // file opening error
-			fprintf(stderr, "%s\n",FOPEN_LOG_ERROR);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	fprintf(pFile_log,"SIGINT (ctrl + c) handled. Program aborted.\n");
-	fclose(pFile_log);
-	pFile_log=NULL;
-
-	// free child pid array
-	if(NULL != pPid_childs){
-		free(pPid_childs);
-		pPid_childs=NULL;
-	}
-
-
-
-
-	exit(EXIT_FAILURE);
-}
-
-
 int search_dir( char *dir_path, char *word){
 
 	int fildes_man;
 	int total=0;
+	int fd;
 
-	pFile_log = fopen(LOG_FILE_NAME,"w");
+	//pFile_log = fopen(LOG_FILE_NAME,"w");
 
+	
+	fd = open("hmenn.log",(O_WRONLY | O_CREAT),FD_MODE);
+	total = search_dir_recursive(dir_path,word,fd);
+	close(fd);
+	
 
-	total = search_dir_recursive(dir_path,word);
-
-	fprintf(pFile_log,"------------------------------------------\n");
-	fprintf(pFile_log,"Found %d times %s is %s dir.\n",total,dir_path,word);
 	freeAll();
 	return total;
 }
@@ -103,11 +94,11 @@ int search_dir( char *dir_path, char *word){
 
 
 
-int search_dir_recursive( char *dir_path, char *word){
+int search_dir_recursive( char *dir_path, char *word,int fd){
 
 	pid_t pid_child=-1;
 	pid_t pid_dead_child=-1;
-	int iTotal_wordnum=0;
+	int iTotal_wordnum=0; // toplam bulunan iterasyon sayisi
 	int fdStatus = -1; // current dirrectory and file status
 	int drStatus = -1;
 	char strPath[PATH_MAX];
@@ -116,7 +107,6 @@ int search_dir_recursive( char *dir_path, char *word){
 
 	if(NULL == (pDir_current = opendir(dir_path))){
 		fprintf(stderr, "[%ld] %s : %s\n",(long)getpid(),DIROPEN_LOG_ERROR,strerror(errno));
-		fprintf(pFile_log, "[%ld] %s : %s\n",(long)getpid(),DIROPEN_LOG_ERROR,strerror(errno));
 		exit_hmenn(1);
 	}
 
@@ -133,76 +123,105 @@ int search_dir_recursive( char *dir_path, char *word){
 			iDir_num,iFile_num,dir_path);
 #endif
 
-	pPid_childs = calloc(sizeof(pid_t),iDir_num);
+	pTChilds = calloc(sizeof(t_child),iDir_num);
 	allSearch = (t_search *)calloc(sizeof(t_search),iFile_num);
 
+	//pipe between process - thread
 	open_pipe_connection(allSearch,iFile_num);
 	rewinddir(pDir_current);
 
+	char strFifoName[MAX_FILE_NAME];
+	int tempFD;
 	while(NULL != (pDirent_current = readdir(pDir_current))){
 		if(strcmp(pDirent_current->d_name,".")!=0 && strcmp(pDirent_current->d_name,"..")!=0){
 			sprintf(strPath,"%s/%s",dir_path,pDirent_current->d_name);
 
+			// HER DOSYAYI BIRER THREAD OKUYACAK VE KAYDEDECEK LOGA
 			if(is_regfile(strPath)){
 				++fdStatus;
 				allSearch[fdStatus].filename = strFiles[fdStatus];
 				allSearch[fdStatus].word = word;
 				allSearch[fdStatus].status = fdStatus;
-				printf("File: %s \n",allSearch[fdStatus].filename);
-				
+				//printf("File: %s \n",allSearch[fdStatus].filename);
 				pthread_create(&(allSearch[fdStatus].tid),NULL,search_with_thread,&allSearch[fdStatus]);
 
-				// TODO : THREAD CONTINUE
-				
+				//  THREAD PARENTE KATILACAK
 			}else if(is_directory(strPath)){
-				continue;
-
+				
+				++drStatus;
+				openFifoConnection(pTChilds,iDir_num,drStatus);
+				// dosyalar icin fork et
 				if(-1 == (pid_child = fork())){
 					fprintf(stderr,"Fork error.");
 					exit_hmenn(0);
 				}
 
-				if(0 == pid_child){
+				if(0 == pid_child){ // asagindan devammmm
 					break;
 				}
 				else{
-					// TODO : OPEN FIFO CONNECTIONS FOR PROCESS
-
+					// read icin parent fifoyu acarr
+					sprintf(strFifoName,"%ld-%d.ff",(long)getpid(),drStatus);
+					pTChilds[drStatus].fd[0]= open(strFifoName,O_RDONLY);
+					pTChilds[drStatus].pid=pid_child;
+					pTChilds[drStatus].status=drStatus;
 				}
-
 			}
 		}
-		//break;
 	}
 
-
-	// child here
+	// child devamm
 	if(pid_child == 0){
 
-
+		pid_t temp;
+		// fifoyu write modunda ac
+		sprintf(strFifoName,"%ld-%d.ff",(long)getppid(),drStatus);
+		pTChilds[drStatus].fd[1] = open(strFifoName,O_WRONLY);
+		if(pTChilds[drStatus].fd[1] == -1) { // hata kontrol
+	      	fprintf(stderr, "[%ld]:failed to open named pipe %s for write: %s\n",
+	             (long)getpid(), strFifoName, strerror(errno));
+	     	exit(1);
+     	}
+     	// recursive cagri icin directory processin fd sini kaydet
+     	// yeni recursive parentin fd sine yazacak
+		temp = pTChilds[drStatus].fd[1];
+		freeAll(); // kullanılanları geri ve
+		search_dir_recursive(strPath,word,temp); // recursive cagri
+		close(temp);
+		unlink(strFifoName);	
+		exit_hmenn(0); //
 	}else // parent here
 	{
-		// first wait for threads and read their outputs to global log file
+		pid_t pid_dead;
+		// once kendi threadlerinde veri oku
 		int i=0;
-		for(i=0;i<=fdStatus;++i){
+		for(i=0;i<=fdStatus;++i){	
 			pthread_join(allSearch[i].tid,NULL);
 			close(allSearch[i].fd[1]);
-			fprintf(pFile_log,"\nFILE : %s    // WORD : %s \n",allSearch[i].filename,allSearch[i].word);
-	        iTotal_wordnum += copyfile(allSearch[i].fd[0],pFile_log);
-	        close(allSearch[i].fd[0]);
+			char str[255];
+			sprintf(str,"FILE : %s - Total :%d\n",allSearch[i].filename,allSearch[i].total);
+	        iTotal_wordnum += allSearch[i].total;
+	        write(fd,&str,strlen(str)*sizeof(char));
+	        copyfile3(allSearch[i].fd[0],fd);
+	        close(allSearch[i].fd[0]);	        
 			printf("Thread %d dead.\n",i);
 		}
 
+
+		while(-1 != (pid_dead = wait(NULL))){
+			char strFifoName[MAX_FILE_NAME];
+			int id = getID(pTChilds,iDir_num,pid_dead);
+			
+			printf("Child[%ld - %d] dead.",(long)pid_dead,id);
+			printf("%d",pTChilds[id].fd[0]);
+			copyfile3(pTChilds[id].fd[0],fd);
+			close(pTChilds[id].fd[0]);
+		}
+
 	}
 
 
-
-
-
-
-
-
-
+	freeAll();
 
 	return iTotal_wordnum;
 
@@ -317,6 +336,7 @@ void * search_with_thread(void *args){
 
 int findOccurencesInFile(int fd,const char* fileName,const char *word){
 
+	char str[30];
   int fdFileToRead; /* okunacak dosya fildesi */
   char buf; /* tek karakter okumalik buffer */
   int i=0;
@@ -353,10 +373,11 @@ arkaya bulununca imleci geriye cek ve devam et. Tum eslesen kelimeleri bul
         coord.found=found;
         coord.row=row;
         coord.column=column;
+        sprintf(str,"%d. %d %d\n",coord.found,coord.row,coord.column);
         #ifdef DEBUG_FILE_READ
         printf("%d. %d %d\n",coord.found,coord.row,coord.column);
         #endif
-        write(fd,&coord,sizeof(coord));
+        write(fd,&str,sizeof(char)*(strlen(str)));
         i=0;
       }
     }else{
@@ -388,15 +409,12 @@ void open_pipe_connection(t_search * arr, int size){
 void freeAll(){
 
 
-	if(NULL != pPid_childs){
-		free(pPid_childs);
-		pPid_childs=NULL;
+	if(NULL != pTChilds){
+		free(pTChilds);
+		pTChilds=NULL;
 	}
 
-	if(NULL != pFile_log){
-		fclose(pFile_log);
-		pFile_log=NULL;
-	}
+	
 
 
 	if(NULL != pDir_current){
@@ -404,7 +422,7 @@ void freeAll(){
 		pDir_current=NULL;
 	}
 
-	pDirent_current=NULL;
+	//pDirent_current=NULL;
 
 	if(NULL != pTh_thread){
 		free(pTh_thread);
@@ -428,6 +446,7 @@ void freeAll(){
 		strDirectories=NULL;
 	}
 
+
 	if(NULL != strFiles){
 		for(i=0;i<iFile_num;++i){
 			if(strFiles[i]!=NULL){
@@ -450,8 +469,78 @@ int copyfile(int fromfd, FILE* out) {
 
    	while (read(fromfd,&coord,sizeof(coord)) > 0){
    		++totalnum;
+   		printf("%d %d %d \n",coord.found,coord.row,coord.column);
       fprintf(out, "%d.  %d  %d\n",coord.found,coord.row,coord.column);
    }
    return totalnum;
 }
 
+int getID(t_child *arr,int size,pid_t pid){
+  int i=0;
+  if(NULL == arr || size <=0)
+    return -1;
+
+  for(i=0;i<size;++i)
+    if(arr[i].pid==pid)
+      return i;
+  return -1;
+}
+
+ssize_t r_read(int fd, void *buf, size_t size) {
+   ssize_t retval;
+   while (retval = read(fd, buf, size), retval == -1 && errno == EINTR) ;
+   return retval;
+}
+
+ssize_t r_write(int fd, void *buf, size_t size) {
+  char *bufp;
+   size_t bytestowrite;
+   ssize_t byteswritten;
+   size_t totalbytes;
+
+   for (bufp = buf, bytestowrite = size, totalbytes = 0;
+        bytestowrite > 0;
+        bufp += byteswritten, bytestowrite -= byteswritten) {
+      byteswritten = write(fd, bufp, bytestowrite);
+      if ((byteswritten) == -1 && (errno != EINTR))
+         return -1;
+      if (byteswritten == -1)
+         byteswritten = 0;
+      totalbytes += byteswritten;
+   }
+   return totalbytes;
+}
+
+int copyfile3(int fromfd, int tofd) {
+   int bytesread;
+   int totalbytes = 0;
+
+   while ((bytesread = readwrite(fromfd, tofd)) > 0)
+      totalbytes += bytesread;
+   return totalbytes;
+}
+
+int readwrite(int fromfd, int tofd) {
+   char buf[1024];
+   int bytesread;
+
+   if ((bytesread = r_read(fromfd, buf, 1024)) < 0)
+      return -1;
+   if (bytesread == 0)
+      return 0;
+   if (r_write(tofd, buf, bytesread) < 0)
+      return -1;
+   return bytesread;
+}
+
+int copyfile2(int fromfd, int tofd) {
+   	t_coordinat coord;
+   	int totalnum=0;
+
+   	while (read(fromfd,&coord,sizeof(t_coordinat)) > 0){
+   		++totalnum;
+   		printf("%d %d %d \n",coord.found,coord.row,coord.column);
+      write(tofd,&coord,sizeof(t_coordinat));
+   }
+   return totalnum;
+}
--- hw4_gFD_Thread/hw4.h
+++ hw4_gFD_Thread/hw4.h
@@ -1,11 +1,13 @@
 #ifndef HW4_131044009
  #define HW4_131044009 1
 
-#define DEBUG_FILE_READ
+//#define DEBUG_FILE_READ 
 
 
-#define BLKSIZE 512
+
 #define FD_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define FIFO_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define MAX_FILE_NAME 20
 #define LOG_FILE_NAME "hmenn.log"
 #define FOPEN_LOG_ERROR "An error occurred while opening the file"
 #define DIROPEN_LOG_ERROR "An error occured while opening the directory"
@@ -19,6 +21,12 @@ typedef struct {
 	int status;
 	int total;
 }t_search;
+
+typedef struct{
+	pid_t pid;
+	int status;
+	int fd[2]; // fifo 
+}t_child;
 
 typedef struct {
 	int found;
@@ -40,7 +48,7 @@ int search_dir( char * dir_path,  char * word);
 
 //recursive directory searcher
 // it is PRIVATE function
-int search_dir_recursive( char *dir_path,  char *word);
+int search_dir_recursive( char *dir_path,  char *word,int fd);
 
 
 // counst number of file and directory in dir_path
@@ -56,8 +64,17 @@ void freeAll();
 
 
 
+int getID(t_child *arr,int size,pid_t pid);
 
 // from course book
 int copyfile(int fromfd, FILE *out);
+
+
+
+
+int copyfile2(int fromfd, int tofd);
+int readwrite(int fromfd, int tofd);
+ssize_t r_write(int fd, void *buf, size_t size);
+ssize_t r_read(int fd, void *buf, size_t size);
 
 #endif
